156a5002e251af7ee6c77abdc0bd89ea72451d9e
israelsilva <israel.bernardes.silva@gmail.com>
2025-05-16 18:24:59 -0300

    add csv parser and create object per row

---
+++ app/csv_reader.py
@@ -0,0 +1,34 @@
+import csv
+import io
+from typing import List
+
+
+class MovieCsv:
+    def __init__(self, year: int, title: str, studios: str, producers: str, winner: bool):
+        self.year = year
+        self.title = title
+        self.studios = studios
+        self.producers = producers
+        self.winner = winner
+
+    @classmethod
+    def read_csv_file(cls, file_path: str) -> List['MovieCsv']:
+        with open(file_path, "r", encoding="utf-8") as f:
+            csv_content = f.read()
+            reader = csv.DictReader(io.StringIO(csv_content), delimiter=';')
+            movies = []
+            for row in reader:
+                try:
+                    winner = row['winner'].strip().lower() == 'yes'
+                    movie = cls(
+                        year=int(row['year']),
+                        title=row['title'],
+                        studios=row['studios'],
+                        producers=row['producers'],
+                        winner=winner
+                    )
+                    movies.append(movie)
+                except Exception as e:
+                    print(f"Error processing CSV line {row}. Error: {str(e)}")
+
+            return movies
--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
+from app.csv_reader import MovieCsv
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    movies = MovieCsv.read_csv_file(file_path="app/data/movielist.csv")
+
     yield
 
 app = FastAPI(title="Golden Raspberry Awards API", lifespan=lifespan)
